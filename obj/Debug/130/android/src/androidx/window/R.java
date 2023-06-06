17ca4dceea476e6db8f2eba5553d683f7c253188
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-06-06 16:41:42 +0800

    Disabled SendOTP button after clicking
    To avoid sending of multiple OTP and lagging

--- obj/Debug/130/android/src/androidx/window/R.java
+++ obj/Debug/130/android/src/androidx/window/R.java
@@ -27,7 +27,7 @@ public final class R {
 		public static final int androidx_window_activity_scope = 0x7f080051;
 		public static final int locale = 0x7f080115;
 		public static final int ltr = 0x7f080118;
-		public static final int rtl = 0x7f0801ac;
+		public static final int rtl = 0x7f0801ad;
 	}
 	public static final class styleable {
 		public static final int[] ActivityFilter = new int[] { 0x7f030026, 0x7f030028 };
